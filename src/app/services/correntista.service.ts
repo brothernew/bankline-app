799b92506f46551ff062fdcb95ef06be68feba9d
ThiagoRP <thirp.impacta@gmail.com>
2022-05-14 01:16:39 -0300

    Update 14/05/2022 - fix problemas no repositório

---
+++ src/app/services/correntista.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8080';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CorrentistaService {
+  constructor(private http: HttpClient) { }
+
+  list(): Observable<any> {
+    return this.http.get(`${baseUrl}/correntistas`);
+  }
+
+  create(correntistas:any): Observable<any> {
+    return this.http.post(`${baseUrl}/correntistas`,correntistas);
+  }
+}
